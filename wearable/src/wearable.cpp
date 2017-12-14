03e1875edca2d1a321afe15c0ef514534293179d
Silversmithe <alexadranly@gmail.com>
2017-12-14 13:25:36 -0800

    created new wearable project for the teensy

---
+++ wearable/src/wearable.cpp
@@ -0,0 +1,21 @@
+/*
+ * FILE:        wearable.cpp
+ *
+ * AUTHOR:      Alexander S. Adranly
+ *
+ * DESCRIPTION: The Unsupervised Parkinson's Disease Assessment wearable
+ *              software for data collection and transmission to the
+ *              server for processing
+ *
+ * NOTES:
+ */
+#include <Arduino.h>
+#include <wearable.hpp>
+
+void setup() {
+    // put your setup code here, to run once:
+}
+
+void loop() {
+    // put your main code here, to run repeatedly:
+}
