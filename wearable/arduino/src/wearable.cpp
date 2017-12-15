56f470ae8a00565684fd754674fc9fc16bcf2312
Silversmithe <alexadranly@gmail.com>
2017-12-14 20:56:22 -0800

    created arduino project, works with UNO

---
+++ wearable/arduino/src/wearable.cpp
@@ -0,0 +1,22 @@
+/*
+ * FILE:        wearable.cpp
+ *
+ * AUTHOR:      Alexander S. Adranly
+ *
+ * DESCRIPTION: Prototyping of the system using an Arduino
+ *              while the teensy is currently in transit
+ */
+
+#include <Arduino.h>
+
+void setup() {
+    // put your setup code here, to run once:
+    Serial.begin(9600);
+
+    Serial.println("Hello World!");
+    
+}
+
+void loop() {
+    // put your main code here, to run repeatedly:
+}
