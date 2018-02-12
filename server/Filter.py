644fe71ec2a431b9abbb8037dae0c49fb553cb2e
samlux <34324601+samlux@users.noreply.github.com>
2018-02-12 14:48:05 -0800

    created skeleton objects for server pipeline

---
+++ server/Filter.py
@@ -0,0 +1,18 @@
+"""
+Filters that process and use the
+packet data to produce and anlaysis
+"""
+class Filter(object):
+
+    def __init__(self):
+        pass
+
+    def process(self, packet):
+        """
+        filter processes a packet to
+        produce a result
+        """
+        pass
+
+class Filter1(Filter):
+ 
