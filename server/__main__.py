3c8699162a6231db51e769945993681a19b2e8b5
Silversmithe <alexadranly@gmail.com>
2018-04-12 18:36:47 -0700

    singe-instance server listen

--- server/__main__.py
+++ server/__main__.py
@@ -11,6 +11,7 @@ by Alexander Adranly, 2018
 
 # !/bin/bash
 import time
+import serial
 from digi.xbee.devices import XBeeDevice
 
 # GLOBALS
@@ -18,8 +19,10 @@ VERSION = 1
 RUNNING = True
 # SERVER
 PORT = "ttyUSB0"
+BAUD_RATE = 9600
 SERVER_NAME = "XB_SERVE"
 DEVICE_NAME = "XB_WEAR"
+
 
 """
 XBEE SERVER 
@@ -32,6 +35,29 @@ def server():
     :return:
     """
     print("starting server...")
+
+    device = XBeeDevice(PORT, BAUD_RATE)
+
+    try:
+        device.open()
+
+        def data_receive_callback(xbee_message):
+            print("From %s >> %s" % (xbee_message.remote_device.get_64bit_addr(),
+                                     xbee_message.data.decode()))
+
+        device.add_data_received_callback(data_receive_callback)
+
+        print("Waiting for data...\n")
+        input()
+
+    except serial.serialutil.SerialException:
+        print("Unable to open {}".format(PORT))
+
+    finally:
+        if device is not None and device.is_open():
+            device.close()
+
+    print("closing server...")
 
 
 """
