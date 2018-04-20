ad78b78b64b796def5906fa411bfb5c867d7154c
Silversmithe <alexadranly@gmail.com>
2018-04-19 23:26:52 -0700

    console rewritten and server can receive info

--- server/__main__.py
+++ server/__main__.py
@@ -1,269 +1,108 @@
 #! /usr/bin/python
 
 """
-server.py
+run_server.py
 
 by Alexander Adranly, 2018
 
-NOTE: use the arg parse library for command-line tools
+
+Building the tools for an effective command line interface
 """
-
-# !/bin/bash
-import os
-import time
-import serial
-from digi.xbee.devices import XBeeDevice, RemoteXBeeDevice, Raw802Device, RemoteRaw802Device
-from digi.xbee.models.address import XBee16BitAddress, XBee64BitAddress
-
-# GLOBALS
-VERSION = 1
-RUNNING = True
-# SERVER
-PORT = "/dev/ttyUSB0"
-BAUD_RATE = 57600
-SERVER_NAME = "SERVE"
-SERVER_64B_ADDR = "0013A2004163FE2F"
-SERVER_16B_ADDR = "FE2F"
-WEAR_64B_ADDR = "0013A2004163FE31"
-WEAR_16B_ADDR = "FE31"
-# SD CARD
-SD_PATH = "/media/iron-fist/WEARV2"
-SD_DATA_PATH = "/media/iron-fist/WEARV2/DATA.txt"
-# MESSAGES
-DATA_ACK_CONNECT = "ACK_CONNECT"
-NULL_ADDR = XBee16BitAddress.from_hex_string("0x0000")
+from server import *
 
 
-"""
-XBEE SERVER 
-"""
+class Command(object):
+
+    def __init__(self, name, help_msg, action):
+        self.__name = name
+        self.__help = help_msg
+        self.__action = action
+
+    def name(self):
+        return self.__name
+
+    def __call__(self, tokens):
+        self.__action(tokens)
+
+    def __str__(self):
+        return self.__name + ('\t' * 3) + self.__help
 
 
-class Wearable(object):
-    
-    def __init__(self):
-        self.id = "unknown"
-        self.address = XBee16BitAddress.from_hex_string("0x0000") 
-        self.received_count = 0
-        self.sent_count = 0
-        self.remote_device = None
+class Console(object):
 
-    def reset(self):
-        self.id = "unknown"
-        self.address = XBee16BitAddress.from_hex_string("0x0000") 
-        self.received_count = 0
-        self.sent_count = 0
-        self.remote_device = None
+    def __init__(self, name):
+        self.__name = name
+        self.__prompt = "> "
+        self.__separator = ' '
+        self.__closer = "goodbye"
+        # list of command objects
+        self.__commands = dict()
+        self.__commands['help'] = Command(name="help", help_msg="description of how to use the commands", action=self.cmd_help)
+        self.__commands['exit'] = Command(name='exit', help_msg='exits the console', action=self.cmd_exit)
+        # control objects
+        self.__run = True
 
-
-# create a device object
-upda_wear = Wearable()
-
-
-def server():
     """
-    Xbee Server
-    :return:
+        SETTERS
     """
-    upda_wear.reset()
 
-    # INIT SERVER
-    print("starting server...")
-     
-    device = Raw802Device(PORT, BAUD_RATE)
-    remote = RemoteRaw802Device(device, x16bit_addr=XBee16BitAddress.from_hex_string(WEAR_16B_ADDR)) 
+    def set_prompt(self, prompt):
+        self.__prompt = prompt
 
-    try:
-        device.open()
+    def set_separator(self, sep):
+        self.__separator = sep
 
+    def set_closer(self, close):
+        self.__closer = close
 
-        def msg_callback(xbee_message):
-            upda_wear.received_count = upda_wear.received_count + 1
-            print("{} >> {}".format(remote.get_16bit_addr(), xbee_message.data.decode()))
-            #device.send_data_async(remote, "hello")
-
-        device.add_data_received_callback(msg_callback)
-
-        print("press enter to stop server...")
-        input()
-
-    except KeyboardInterrupt:
-        pass # just escaping the loop
-
-    except serial.serialutil.SerialException:
-        print("Unable to open {}".format(PORT))
-
-    finally:
-        if device is not None and device.is_open():
-            device.close()
-
-    print("closing server...")
-
-
-"""
-Simple UPDA CONSOLE
-"""
-
-
-def stats(tokens):
     """
-    :param tokens:
-    :return:
+        COMMANDS
     """
-    print("stats from previous server session...")
-    print("previosly linked with: {}".format(upda_wear.id))
-    print("address: {}".format(upda_wear.address))
-    print("received messages: {}".format(upda_wear.received_count))
 
+    def add_command(self, command):
+        if not type(command) is Command:
+            print("invalid command to add")
+            return
 
-def start(tokens):
+        if not command.name() in self.__commands:
+            self.__commands[command.name()] = command
+
+    def cmd_help(self, tokens):
+        print("usage: {}\n".format(self.__name))
+        for cmd in self.__commands.items():
+            print(cmd[1])
+
+    def cmd_exit(self, tokens):
+        self.__run = False
+
     """
-    start server
-    start server with render
-    :param tokens:
-    :return:
+        RUNNING SYSTEM
     """
-    if len(tokens) <= 1:
-        print("no object specified...")
-        return
 
-    if tokens[1] == "server":
-        # start server procedure
-        server()
+    def run(self):
+        try:
+            while self.__run:
+                tokens = input(self.__prompt).lower().split(sep=self.__separator)
+                # print(tokens)
+                if tokens[0] in self.__commands:
+                    # running command
+                    self.__commands[tokens[0]](tokens)
 
-    else:
-        print("unknown object to start...")
+                else:
+                    print("unknown cmd '{}'".format(tokens[0]))
 
+                print()
 
-def load(tokens):
-    """
-    :param tokens:
-    :return:
-    """
-    print("downloading patient information from sd...")
-    if not os.path.isdir(SD_PATH):
-        # cannot find the SD card
-        print("SD card {} does not exist".format(SD_PATH))
-    
-    else:
-        # found the SD card
-        # check for the DATA file
-        print("SD card {} found".format(SD_PATH)) 
-        if not os.path.exists(SD_DATA_PATH):
-            print("cannot continue: {} not found".format(SD_DATA_PATH))
-        else:
-            # download file
-            with open(SD_DATA_PATH, 'r') as dfile:
-                content = dfile.read()
-                content = content.split(sep='----- datafile -----')
+        except KeyboardInterrupt:
+            pass
 
-                print("{} sets of patient data found".format(len(content)-1))
-                print("downloading patient data")
-
-                bar_size = 50
-
-                if not (len(content)-1 <= 0):
-                    step = int(bar_size/(len(content)-1))
-
-                    # how many patient files exist so far
-                    num_patients = len([name for name in os.listdir('./data')]) 
-
-                    # download files
-                    for i in range(1, len(content)):
-                        print('|','%'*(step*i), ' '*(bar_size - (step*i)), '|', end='\r')
-                        try:
-                            f = open("data/patient-{}.txt".format(str(i+num_patients)), 'w')
-                            f.write(content[i])
-                            f.close()
-
-                        except BlockingIOError:
-                            print("\nerror: could not open new patient file")
-                            break
-
-                    else:
-                        print("\ndownload complete!")
-
-            with open(SD_DATA_PATH, 'w') as dfile:
-                print("wiping SD card data...")
-                try:
-                    dfile.truncate()
-                    print("process complete!")
-
-                except OSError:
-                    print("error: could not clear SD card")
-
-        if len(tokens) > 1:
-            if tokens[1] == 'and' and tokens[2] == 'process':
-                print("\nloading patient data into scoring process...")
-                # pass data off to evaluation
-
-            else:
-                print("undefined load conjunction")
-                print("try: 'load and process'")
-
-
-def list_items(tokens):
-    """
-    :param tokens:
-    :return:
-    """
-    if len(tokens) <= 1:
-        print("specify an object to list:")
-        print("patients")
-        return
-
-    if tokens[1] == 'patients':
-        patients = [name for name in os.listdir('./data')]
-        print("number of patient records on disk: {}".format(len(patients)))
-        # list all the files
-        print("./data:")
-        for i in patients:
-            print(i)
-
-    else:
-        print("unknown object to list...")
-
-
-def list_cmds(tokens):
-    """
-    :param tokens:
-    :return:
-    """
-    print(" help\t\t\texplains what all the commands do\n",
-          "quit\t\t\tterminates the program\n",
-          "start server\t\tinitialize a server to read xbee\n",
-          "stats\t\t\tinformation on overall server stats\n",
-          "load\t\t\tdownload patient information from a certified SD card\n",
-          "list <object>\t\tlists all items wrt object specified. try 'patients'")
-
-
-def terminate(tokens):
-    """
-    :param tokens:
-    :return:
-    """
-    global RUNNING
-    RUNNING = False
-
-
-command_list = {
-    "quit": terminate,
-    "exit": terminate,
-    "start": start,
-    "help": list_cmds,
-    "stats": stats,
-    "load": load,
-    "list": list_items
-}
+        finally:
+            print(self.__closer)
 
 
 def main():
-    """
-    create a main console that is responsible for the gathering and control of the
-    server
-    :return:
-    """
-    print('*'*50)
+
+    print('*' * 50)
     print("\n  .----.-----.-----.-----.\n",
           "/      \     \     \     \\\n",
           "|  \/    |     |   __L_____L__\n",
@@ -277,30 +116,26 @@ def main():
           "|                  |\n",
           "|                  |\n\n")
 
-    print("Unsupervised Parkinson's Disease Assessment Console")
+    print("UPDA Console")
     print("Version {}".format(VERSION), '\n')
     print('*' * 50, '\n')
 
-    while RUNNING:
-        try:
-            message = input(">>> ")
+    console = Console("UPDA Console")
 
-            tokens = message.lower().split(sep=" ")
+    # generate all commands
+    start_cmd = Command(name='start', help_msg='start a subroutine. ex: start server', action=start)
+    stats_cmd = Command(name='stat', help_msg='produce statistics about the previous server runtime', action=stats)
+    load_cmd = Command(name='load', help_msg='load data from the sd card and write to the server', action=load)
+    list_cmd = Command(name='list', help_msg='list specified items. ex: list patients', action=list_items)
 
-            if tokens[0] in command_list:
-                # in the command list
-                command_list[tokens[0]](tokens)
-            else:
-                # not in the command list
-                print("cannot process '{}'".format(message))
+    # add commands
+    console.add_command(start_cmd)
+    console.add_command(stats_cmd)
+    console.add_command(load_cmd)
+    console.add_command(list_cmd)
 
-            print()
-
-        except KeyboardInterrupt:
-            print()
-            break
-
-    print("goodbye...")
+    # console.run()
+    console.run()
 
 
 if __name__ == '__main__':
---
+++ server/server.py
@@ -0,0 +1,233 @@
+#! /usr/bin/python
+
+"""
+run_server.py
+
+by Alexander Adranly, 2018
+
+NOTE: use the arg parse library for command-line tools
+"""
+
+# !/bin/bash
+import os
+import time
+import serial
+from digi.xbee.devices import XBeeDevice, RemoteXBeeDevice, Raw802Device, RemoteRaw802Device
+from digi.xbee.models.address import XBee16BitAddress, XBee64BitAddress
+
+# GLOBALS
+VERSION = 1
+RUNNING = True
+# SERVER
+PORT = "/dev/ttyUSB0"
+BAUD_RATE = 57600
+SERVER_NAME = "SERVE"
+SERVER_64B_ADDR = "0013A2004163FE2F"
+SERVER_16B_ADDR = "FE2F"
+WEAR_64B_ADDR = "0013A2004163FE31"
+WEAR_16B_ADDR = "FE31"
+# SD CARD
+SD_PATH = "/media/iron-fist/WEARV2"
+SD_DATA_PATH = "/media/iron-fist/WEARV2/DATA.txt"
+
+
+"""
+XBEE SERVER 
+"""
+
+
+class Wearable(object):
+    
+    def __init__(self):
+        self.id = "unknown"
+        self.address = XBee16BitAddress.from_hex_string("0x0000") 
+        self.received_count = 0
+        self.sent_count = 0
+        self.remote = None
+        self.runtime = 0
+
+    def reset(self):
+        self.id = "unknown"
+        self.address = XBee16BitAddress.from_hex_string("0x0000") 
+        self.received_count = 0
+        self.sent_count = 0
+        self.remote = None
+        self.runtime = 0
+
+
+"""
+XBee Wearable Devices
+"""
+upda_wear = Wearable()
+
+
+"""
+Server
+"""
+
+
+def run_server():
+    """
+    Xbee Server
+    :return:
+    """
+    # INIT SERVER
+    print("starting run_server...")
+    upda_wear.reset()
+
+    server = Raw802Device(PORT, BAUD_RATE)
+    upda_wear.id = "UPDA-WEAR-1"
+    upda_wear.address = XBee16BitAddress.from_hex_string(WEAR_16B_ADDR)
+    upda_wear.remote = RemoteRaw802Device(server, upda_wear.address)
+    start_time = time.time()
+
+    try:
+        # might not want this
+        if not os.path.exists('./data'):
+            print("unable to find './data' to store information")
+            raise KeyboardInterrupt
+
+        server.open()
+
+        def msg_callback(message):
+            upda_wear.received_count = upda_wear.received_count + 1
+            # register the device
+            print("{} >> {}".format(server.get_16bit_addr(), message.data.decode()))
+            # pass information off to a buffer
+
+        server.add_data_received_callback(msg_callback)
+
+        print("press enter to stop run_server...")
+        input()
+
+    except KeyboardInterrupt:
+        pass  # just escaping the loop
+
+    except serial.serialutil.SerialException:
+        print("Unable to open {}".format(PORT))
+
+    finally:
+        if server is not None and server.is_open():
+            upda_wear.runtime = time.time() - start_time
+            server.close()
+
+    print("closing run_server...")
+
+
+"""
+Simple UPDA CONSOLE
+"""
+
+
+def stats(tokens):
+    """
+    :param tokens:
+    :return:
+    """
+    print("stats from previous run_server session...")
+    print("previously linked with: {}".format(upda_wear.id))
+    print("address: {}".format(upda_wear.address))
+    print("received messages: {}".format(upda_wear.received_count))
+    print("server runtime: {}".format(upda_wear.received_count))
+
+
+def start(tokens):
+    """
+    start run_server
+    start run_server with render
+    :param tokens:
+    :return:
+    """
+    if len(tokens) <= 1:
+        print("no object specified...")
+        return
+
+    if tokens[1] == "server":
+        # start run_server procedure
+        run_server()
+
+    else:
+        print("unknown object to start...")
+
+
+def load(tokens):
+    """
+    :param tokens:
+    :return:
+    """
+    print("downloading patient information from sd...")
+    if not os.path.isdir(SD_PATH):
+        # cannot find the SD card
+        print("SD card {} does not exist".format(SD_PATH))
+    
+    else:
+        # found the SD card
+        # check for the DATA file
+        print("SD card {} found".format(SD_PATH)) 
+        if not os.path.exists(SD_DATA_PATH):
+            print("cannot continue: {} not found".format(SD_DATA_PATH))
+        else:
+            # download file
+            with open(SD_DATA_PATH, 'r') as dfile:
+                content = dfile.read()
+                content = content.split(sep='----- datafile -----')
+
+                print("{} sets of patient data found".format(len(content)-1))
+                print("downloading patient data")
+
+                bar_size = 50
+
+                if not (len(content)-1 <= 0):
+                    step = int(bar_size/(len(content)-1))
+
+                    # how many patient files exist so far
+                    num_patients = len([name for name in os.listdir('./data')]) 
+
+                    # download files
+                    for i in range(1, len(content)):
+                        print('|','%'*(step*i), ' '*(bar_size - (step*i)), '|', end='\r')
+                        try:
+                            f = open("data/patient-{}.txt".format(str(i+num_patients)), 'w')
+                            f.write(content[i])
+                            f.close()
+
+                        except BlockingIOError:
+                            print("\nerror: could not open new patient file")
+                            break
+
+                    else:
+                        print("\ndownload complete!")
+
+            with open(SD_DATA_PATH, 'w') as dfile:
+                print("wiping SD card data...")
+                try:
+                    dfile.truncate()
+                    print("process complete!")
+
+                except OSError:
+                    print("error: could not clear SD card")
+
+
+def list_items(tokens):
+    """
+    :param tokens:
+    :return:
+    """
+    if len(tokens) <= 1:
+        print("specify an object to list:")
+        print("patients")
+        return
+
+    if tokens[1] == 'patients':
+        if os.path.exists('./data'):
+            patients = [name for name in os.listdir('./data')]
+            print("number of patient records on disk: {}".format(len(patients)))
+            # list all the files
+            print("./data:")
+            for i in patients:
+                print(i)
+        else:
+            print("cannot find path './data'")
+
+    else:
+        print("unknown object to list...")
