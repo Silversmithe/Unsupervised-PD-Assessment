e3cbf5f4a6c3a86355c486b81db11900832c477f
Silversmithe <alexadranly@gmail.com>
2018-04-27 02:23:54 -0700

    use start instead of run to start thread

--- server/server.py
+++ server/server.py
@@ -189,7 +189,7 @@ def run_server():
         num_patients = len([name for name in os.listdir('./data')])
 
         instance_manager = InstanceLoader(num_patients)
-        instance_manager.start()
+        instance_manager.start()  # start the thread
         server.open()
 
         def msg_callback(message):
