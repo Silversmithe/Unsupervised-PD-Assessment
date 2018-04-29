94e9191d5c28bda1af3fb48e3801a64a8ff2ccd0
Yousef-Zoumot <yzoumot17@gmail.com>
2018-04-29 15:49:17 -0700

    Update score.py file with new functions

--- server/Score.py
+++ server/Score.py
@@ -3,7 +3,6 @@ Object responsible for UPDRS scoring
 of
 """
 
-
 class Score(object):
 
     def __init__(self):
@@ -27,3 +26,23 @@ class Score(object):
     # display
     def display_score(self):
         pass
+
+
+    def get_input(textfile):
+        text_file = open(textfile, "r")
+        lines = text_file.read().split("\n")
+        total_inputs = len(lines) - 1
+        text_file.close()
+
+        dataset = [[float(0) for x in range(1)] for y in range(total_inputs)]
+        for i in range(total_inputs):
+        #split data points of each instance
+            dataset[i] = lines[i]
+        # print(dataset_ftaps[1][0])
+        return dataset
+
+
+    def choose_frequency(dataset1, dataset2, dataset3, dataset4):
+        
+
+    def count_taps(dataset_taps, dataset_interupts):
