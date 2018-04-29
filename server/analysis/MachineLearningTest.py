d76a4927db8dd6b00f5e7590c9a78ead74836255
Yousef-Zoumot <yzoumot17@gmail.com>
2018-04-29 16:08:52 -0700

    add sigmoid function and get_prediction functions to Machine Learning file

---
+++ server/analysis/MachineLearningTest.py
@@ -0,0 +1,29 @@
+"""
+Object responsible for generating test outputs
+by multiplying the new data matrix with our
+calculated column weights in our Machine Learning
+model
+"""
+
+class MachineLearningTest(object):
+
+    """
+    Sigmoid maps a number between 1 and 0
+    Sigmoid function:
+                    1
+    sigmoid(x)= ----------
+                1 + e^(-x)
+    """
+    def sigmoid(temp_in):
+        return np.float64(1 / (1 + np.exp( - temp_in)))
+
+
+    """
+    By multiplying the new inputs with our calculated
+    column weights, we generate a column of prediction
+    values to determine whether or not an action occured
+    """
+    def get_predictions(inputs, weights):
+        return sigmoid(np.matmul(inputs, weights))
+
+    
