ea0f73685a1722a3e2828cad27909d0dd3b3b537
Yousef-Zoumot <yzoumot17@gmail.com>
2018-05-06 02:19:08 -0700

    debugging

--- server/Score.py
+++ server/Score.py
@@ -970,7 +970,7 @@ class Score(object):
             # freq = 0 : df : fs - df
             s2 = slice(beginning_index, end_index, 1)
             mag_tremor = np.linalg.norm(true_data[s2][0:])
-            mag_tremor_max = max(mag_tremor)
+            mag_tremor_max = np.max(mag_tremor)
             for i in range(24):
                 if mag_tremor_max(j) > tremor_amp:
                     tremor_count = tremor_count+1
