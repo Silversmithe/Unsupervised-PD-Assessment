77e20327df3d6a2ac9ab1a9f9ec8a462b191de42
Silversmithe <alexadranly@gmail.com>
2018-06-03 12:01:03 -0700

    gravity filter update

--- server/analysis/GravityFilter.py
+++ server/analysis/GravityFilter.py
@@ -4,6 +4,18 @@ GRAVITY FILTER
 A filter that can find the direction of gravity given the orientation of the hand. This
 direction can be used to discern when a data is resting their hand or they are
 using their hand
+
+RM_hand=[2.*(hqi.*hqk+hqj.*hqr).*g,2.*s.*(hqj.*hqk-hqi.*hqr).*g,(hqr.^2-hqi.^2-hqj.^2+hqk.^2).*g];
+
+b = i
+a = r
+c = j
+d = k
+
+2(bd+ac) * g
+2(cd - ab) * g
+(a^2 - b^2 - c^2 + d^2) * g
+
 """
 from math import *
 import numpy as np
@@ -12,7 +24,7 @@ from MatrixBuilder import extract
 
 class GravityFilter(object):
 
-    GRAVITY = np.matrix([[0.0], [0.0], [-9.81]])
+    GRAVITY = -9.81
 
     def __init__(self, filename):
         self.__filename = filename
@@ -22,35 +34,20 @@ class GravityFilter(object):
         # go to raw file with quads
         outfile = open("{}/gravity.txt".format(self.__filename), "w")
 
+        def calc_accel(q, a):
+            """
+            :param q: [r, i, j, k] : quaternions
+            :param a: [x, y, z] : raw acceleration
+            :return: (list) "true" acceleration
+            """
+            x_comp = 2.0 * (q[0, 1] * q[0, 3] + q[0, 0] * q[0, 2]) * self.GRAVITY
+            y_comp = 2.0 * (q[0, 2] * q[0, 3] - q[0, 0] * q[0, 1]) * self.GRAVITY
+            z_comp = (pow(q[0, 0], 2.0) - pow(q[0, 1], 2.0) - pow(q[0, 2], 2.0) + pow(q[0, 3], 2.0)) * self.GRAVITY
+            return [x_comp + a[0, 0], y_comp + a[0, 1], z_comp + a[0, 2]]
+
         rawAccel = extract(self.__filename, 'HA', 'TA', 'PA', 'RA', 'Q')
         rawAccelMat = np.matrix(rawAccel).transpose()
 
-        # print("!!! BEFORE !!!")
-        # get all the accelerations
-        # with open("{}/raw.txt".format(self.__filename), "r") as rawfile:
-        #     for line in rawfile:
-        #         vals = line.split(sep=' ')
-        #         # HAND
-        #         Hx = float(vals[2])
-        #         Hy = float(vals[3])
-        #         Hz = float(vals[4])
-        #         # THUMB
-        #         Tx = float(vals[11])
-        #         Ty = float(vals[12])
-        #         Tz = float(vals[13])
-        #         # POINT
-        #         Px = float(vals[20])
-        #         Py = float(vals[21])
-        #         Pz = float(vals[22])
-        #         # RING
-        #         Rx = float(vals[29])
-        #         Ry = float(vals[30])
-        #         Rz = float(vals[31])
-        #
-        #         qh = [float(vals[38]), float(vals[39]), float(vals[40]), float(vals[41])]
-        #         qt = [float(vals[42]), float(vals[43]), float(vals[44]), float(vals[45])]
-        #         qp = [float(vals[46]), float(vals[47]), float(vals[48]), float(vals[49])]
-        #         qr = [float(vals[50]), float(vals[51]), float(vals[52]), float(vals[53])]
         try:
             for line in range(0, len(rawAccel[0])):
 
@@ -69,67 +66,24 @@ class GravityFilter(object):
                 qr = rawAccelMat[line, 24:28]
 
                 # generate rotation matrix
-                rot_h = self.generate_rot_mat(qh)
-                rot_t = self.generate_rot_mat(qt)
-                rot_p = self.generate_rot_mat(qp)
-                rot_r = self.generate_rot_mat(qr)
-
-                # multiply gravity by rotation matrix to get gravity in new reference frame
-                rr_h = np.matmul(rot_h, self.GRAVITY)
-                rr_t = np.matmul(rot_t, self.GRAVITY)
-                rr_p = np.matmul(rot_p, self.GRAVITY)
-                rr_r = np.matmul(rot_r, self.GRAVITY)
+                true_h = calc_accel(qh, rawAccelMat[line, 0:3])
+                true_t = calc_accel(qt, rawAccelMat[line, 3:6])
+                true_p = calc_accel(qp, rawAccelMat[line, 6:9])
+                true_r = calc_accel(qr, rawAccelMat[line, 9:12])
 
                 # store scores
                 # HAND
                 # print(rr_h.shape)
                 # FOR EACH LINE:
                 # HAx HAy HAz
-                outfile.write(str(rr_h.item((0, 0)) + rawAccelMat[line, 0]) + ' ')
-                outfile.write(str(rr_h.item((1, 0)) + rawAccelMat[line, 1]) + ' ')
-                outfile.write(str(rr_h.item((2, 0)) + rawAccelMat[line, 2]) + ' ')
-
+                outfile.write("{} {} {} ".format(true_h[0], true_h[1], true_h[2]))
                 # THUMB
-                outfile.write(str(rr_t.item((0, 0)) + rawAccelMat[line, 3]) + ' ')
-                outfile.write(str(rr_t.item((1, 0)) + rawAccelMat[line, 4]) + ' ')
-                outfile.write(str(rr_t.item((2, 0)) + rawAccelMat[line, 5]) + ' ')
-
+                outfile.write("{} {} {} ".format(true_t[0], true_t[1], true_t[2]))
                 # POINT
-                outfile.write(str(rr_p.item((0, 0)) + rawAccelMat[line, 6]) + ' ')
-                outfile.write(str(rr_p.item((1, 0)) + rawAccelMat[line, 7]) + ' ')
-                outfile.write(str(rr_p.item((2, 0)) + rawAccelMat[line, 8]) + ' ')
-
+                outfile.write("{} {} {} ".format(true_p[0], true_p[1], true_p[2]))
                 # RING
-                outfile.write(str(rr_r.item((0, 0)) + rawAccelMat[line, 9]) + ' ')
-                outfile.write(str(rr_r.item((1, 0)) + rawAccelMat[line, 10]) + ' ')
-                outfile.write(str(rr_r.item((2, 0)) + rawAccelMat[line, 11]))
-                outfile.write('\n')
+                outfile.write("{} {} {}\n".format(true_r[0], true_r[1], true_r[2]))
 
         finally:
             # print("!!! AFTER !!!")
             outfile.close()
-
-    @staticmethod
-    def generate_rot_mat(q):
-        """
-
-        :param q:
-        :return:
-        """
-        # get rid of q[0]^2 from value
-        # print("{} {} {}".format(q[1], q[2], q[3]))
-        total = (pow(q[0, 1], 2.0) + pow(q[0, 2], 2.0) + pow(q[0, 3], 2.0))
-
-        # if total == 0:
-        #     s = 0
-        # else:
-        #     s = 1.0/total
-        s = 1
-
-        r = [
-                [1 - 2 * s * (pow(q[0, 2], 2.0) + pow(q[0, 3], 2.0)), 2 * s * (q[0, 1] * q[0, 2] - q[0, 3] * q[0, 0]), 2 * s * (q[0, 1] * q[0, 3] + q[0, 2] * q[0, 0])],
-                [2 * s * (q[0, 1] * q[0, 2] + q[0, 3] * q[0, 0]), 1 - 2 * s * (pow(q[0, 1], 2.0) + pow(q[0, 3], 2.0)), 2 * s * (q[0, 2] * q[0, 3] - q[0, 1] * q[0, 0])],
-                [2 * s * (q[0, 1] * q[0, 3] - q[0, 2] * q[0, 0]), 2 * s * (q[0, 2] * q[0, 3] + q[0, 1] * q[0, 0]), 1 - 2 * s * (pow(q[0, 1], 2.0) + pow(q[0, 3], 2.0))]
-        ]
-
-        return np.matrix(r)
